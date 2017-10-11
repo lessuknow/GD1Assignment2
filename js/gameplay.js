6ca1b761d72d3857a605a03d89997df451f71588
lessuknow <chenc18@rpi.edu>
2017-10-11 15:24:38 -0400

    More changes. Added a working backpack button; moved the inventory to the right hand side of things. You click on the backpack button to display it, and then just click off of the backpack's inventory to get it to go away. easy!

--- js/gameplay.js
+++ js/gameplay.js
@@ -8,34 +8,56 @@ gameplayState.prototype.preload = function(){
 	game.load.image("invTile","assets/invTile.png");
 	game.load.image("bkpk","assets/backpack.png");
 	game.load.image("bkpkMenu","assets/bkpkMenu.png");
+	game.load.image("scrollTile","assets/scrollTile.png");
 }
+
+
 
 gameplayState.prototype.create = function(){
 	game.add.sprite(0,0,"sky");
 	
-	//use this for placing the actual images fo the stuff inside the inventory tiles in the backpack
-	const BACKPACK_TILE_BORDER = 25;
-	const WINDOW_WIDTH = 750;
-	const TILE_WIDTH = 150;
+	let bkpk = game.add.sprite(750-150,1334-150,"bkpk");
+	this.bkpkOpen = false;
 	
-	let bkpkMenu = game.add.sprite(750*3/4,1334-200,"bkpkMenu");
+	this.bkpkMenu = game.add.sprite(750*3/4,0,"bkpkMenu");	
+	this.bkpkMenu.visible = false;
 	
 	this.invTiles = game.add.group();
 	this.canDrag = game.add.group();
 	
-	for(i=0;i<4;i++){
-		let invTile = this.invTiles.create(0+BACKPACK_TILE_BORDER + (i * TILE_WIDTH),1334-200+BACKPACK_TILE_BORDER,"invTile");
+	//openBackpack();
+	
+			
+	this.bkpkMenu.inputEnabled = true;	
+	//backpack stuff; create and hide
+	//use this for placing the actual images fo the stuff inside the inventory tiles in the backpack
+	const BACKPACK_TILE_BORDER = 17;
+	const WINDOW_WIDTH = 750;
+	const TILE_HEIGHT = 150;
+	const SCROLL_TILE_HEIGHT = 75;
+	
+	//Backpack 'open' code; displays a column with tiels that hold the inventory.
+	this.scrollUp = game.add.sprite(BACKPACK_TILE_BORDER + WINDOW_WIDTH*3/4, 0, "scrollTile");
+	this.scrollDown = game.add.sprite(BACKPACK_TILE_BORDER + WINDOW_WIDTH*3/4, 1334-SCROLL_TILE_HEIGHT, "scrollTile");
+	
+	this.invTiles.create(BACKPACK_TILE_BORDER + WINDOW_WIDTH*3/4 , SCROLL_TILE_HEIGHT+BACKPACK_TILE_BORDER+BACKPACK_TILE_BORDER + TILE_HEIGHT,"invTile");
+	
+	for(i=0;i<7;i++){
+		this.invTiles.create(BACKPACK_TILE_BORDER + WINDOW_WIDTH*3/4 , SCROLL_TILE_HEIGHT+BACKPACK_TILE_BORDER+BACKPACK_TILE_BORDER*i + TILE_HEIGHT*i,"invTile");
 	}
+
+	this.scrollUp.visible = false;
+	this.scrollDown.visible = false;
+	this.invTiles.visible = false;
+	
 	let item = this.canDrag.create(50,350,"item");
 	
-	this.invTiles.visible = true;
 	
 	item.origX = item.x;
 	item.origY = item.y;
 	
 	//we're going to use 2 arrays to store a key/value. like a super ghetto dictionary
 	//keys = sprite, values = text
-	//item.overlapKey = [invTile]
 	item.overlapKey = ["shoe"];
 	item.overlapValue = ["inv"];
 	
@@ -43,7 +65,7 @@ gameplayState.prototype.create = function(){
 	item.checkOverlap = function(){
 		for(i = 0;i < this.overlapKey.length;i++)
 		{
-			if(checkOverlap(this, this.overlapKey[i])){
+			if( (this, this.overlapKey[i])){
 				return this.overlapValue[i];
 			}
 		}
@@ -53,22 +75,39 @@ gameplayState.prototype.create = function(){
 	item.input.enableDrag();
 	item.events.onDragStop.add(checkIfReturnToOrigin, this);
 	
-	//we only need one backpack...
-	let bkpk = game.add.sprite(150,150,"bkpk");
-	
-	
-	bkpkMenu.inputEnabled = true;
-	//this prevents buttons under from being pressed
-	
-	//bkpkMenu.visible = false;
-	//hide the backpack menu initially
 	
 	bkpk.inputEnabled = true;
-	bkpk.events.onInputDown.add(toggleBackpackMenu, bkpkMenu);
+	bkpk.events.onInputDown.add(openBackpack, this);
 	
 }
 
-gameplayState.prototype.toggleInvSquares
+function openBackpack(){
+	
+	this.bkpkOpen=true;
+	this.bkpkMenu.visible = true;
+	
+	
+	this.scrollUp.visible = true;
+	this.scrollDown.visible = true;
+	this.invTiles.visible = true;
+
+}
+
+
+gameplayState.prototype.update = function(){
+	if(game.input.mousePointer.isDown)
+	{
+		if(game.input.mousePointer.x <750-190 && this.bkpkOpen==true){
+			bkpkOpen = false;
+			this.bkpkMenu.visible = false;	
+			this.scrollUp.visible = false;
+			this.scrollDown.visible = false;
+			this.invTiles.visible = false;
+	
+			
+		}
+	}
+}
 
 function toggleBackpackMenu(){
 	
@@ -108,7 +147,3 @@ function checkOverlap(spriteA, spriteB) {
 }
 
 
-
-gameplayState.prototype.update = function(){
-	
-}
